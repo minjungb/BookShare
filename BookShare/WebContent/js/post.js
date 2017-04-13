c239a90e70762d904042cd02a611df1e0af9ad45
epizzo712 <izbiz12@gmail.com>
2017-04-13 19:44:33 -0400

    updated post js (creates correct num of posts from database)

--- BookShare/WebContent/js/post.js
+++ BookShare/WebContent/js/post.js
@@ -1,11 +1,27 @@
 $(document).ready(function(){
-  for(i = 0; i < 4; i++) {
-  	var html= '<div id="div'+ i +'" />' +
-	  			  '<h3>Book title</h3>' +
-	  			  '<p>ISBN:</p>' +
-	  			  '<p>Edition:</p>' +
-	  			  '<p>Description</p>' +
-	  		   '</div>';
-    $('#postings').append(html);
-  }
+	var funct = "fill";
+	$.ajax({
+		type: 'POST',
+		url: 'http://localhost:8080/BookShare/PostServlet',
+		dataType: 'json',
+		data : {
+			funct: funct
+		},
+		success : function(result){
+			var num = result;
+			for(i = 0; i < num; i++) {
+				var html= '<div class="post" id="div'+ (num-i) +'" />' +
+					  			  '<h3>Book title</h3>' +
+					  			  '<p>ISBN:</p>' +
+					  			  '<p>Edition:</p>' +
+					  			  '<p>Description</p>' +
+					  		   '</div>';
+				$('#postings').append(html);
+			}
+			
+		},
+		error : function(jqXHR, status){
+			console.log(status);
+		}
+	});
 }); 
